74ac8f9c8f518641182575c9bb3d9d677666cd82
SaadAhmedGit <saadahmedcronos@gmail.com>
2023-02-15 21:26:21 +0500

    typescript setup for server

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import Express from 'express';
+
+const app = Express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`Server is up and running at https://localhost:${PORT}.`);
+});

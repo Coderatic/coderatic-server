0c5521e8dfd29a79dc33110e5a24807a1346e9aa
redripper480 <jzoraz@gmail.com>
2023-02-16 22:59:59 +0500

    Auth Form

---
+++ client/src/components/AuthForm.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <form>
+        <label>Usrname: </label>
+        <input type="text" name="username">
+        <label>Password: </label>
+        <input type="text" name="password">
+        <input type="submit" value="Login">
+    </form>
+</div>
+</template>
+
+<script lang="ts">
+export default{
+    name: "AuthForm",
+}
+</script>
+
+<style>
+
+</style>

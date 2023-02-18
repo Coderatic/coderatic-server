6ee3d996e93c4f21a386ae85d70303b042c4bf33
redripper480 <jzoraz@gmail.com>
2023-02-18 18:06:32 +0500

    Integrate tailwind

--- client/src/components/AuthForm.vue
+++ client/src/components/AuthForm.vue
@@ -2,8 +2,8 @@
   <div>
     <form>
       <label>Username: </label>
-      <input type="text" name="username" v-model="username" />
-      <label>Password: </label>
+      <input  type="text" name="username" v-model="username" />
+      <label class="text-yellow-100 text-lg">Password: </label>
       <input type="text" name="password" v-model="password" />
       <input type="submit" value="register" @click="register" />
     </form>
@@ -13,7 +13,7 @@
 <script lang="ts">
 import authService from "../services/authService";
 export default {
-  name: "AuthForm",
+  name: "AuthForm", 
   data() {
     return {
       username: "",

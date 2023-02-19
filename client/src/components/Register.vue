7a381f2cc90959320d9d86b4b3b65264b606a91e
SaadAhmedGit <saadahmedcronos@gmail.com>
2023-02-19 12:56:26 +0500

    Accepted changes from Zoraz

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -2,17 +2,8 @@
   <div>
     <form>
       <label>Username: </label>
-<<<<<<< HEAD:client/src/components/Register.vue
-      <input type="text" name="username" v-model="username" />
-      <br />
-      <label>Email: </label>
-      <input type="email" name="Email" v-model="email" />
-      <br />
-      <label>Password: </label>
-=======
       <input  type="text" name="username" v-model="username" />
       <label class="text-yellow-100 text-lg">Password: </label>
->>>>>>> 6ee3d996e93c4f21a386ae85d70303b042c4bf33:client/src/components/AuthForm.vue
       <input type="text" name="password" v-model="password" />
       <br />
       <input type="submit" value="register" @click="registerUser" />
@@ -23,11 +14,7 @@
 <script lang="ts">
 import authService from "../services/authService";
 export default {
-<<<<<<< HEAD:client/src/components/Register.vue
-  name: "Register",
-=======
   name: "AuthForm", 
->>>>>>> 6ee3d996e93c4f21a386ae85d70303b042c4bf33:client/src/components/AuthForm.vue
   data() {
     return {
       username: "",

a2b8872907abbf433601c861d9d78ab1405111f2
redripper480 <jzoraz@gmail.com>
2023-02-19 14:21:23 +0500

    Center Register form

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>
-    <form>
-      <label>Username: </label>
-      <input  type="text" name="username" v-model="username" />
-      <label class="text-yellow-100 text-lg">Password: </label>
-      <input type="text" name="password" v-model="password" />
-      <br />
-      <input type="submit" value="register" @click="registerUser" />
+  <div class="w-screen h-screen flex flex-wrap justify-center content-center">
+
+    <form class="h-fit">
+      <div class="flex flex-col flex-wrap justify-center content-center px-8 py-8">
+      <div>
+      <label class="text-black text-lg block">Username: </label>
+      <input class="border-2 border-black rounded"  type="text" name="username" v-model="username" />
+    </div>
+    <div>
+      <label class="text-black text-lg block">Password: </label>
+      <input class="border-2 border-black rounded" type="text" name="password" v-model="password" />
+    </div>
+      <input class="border-2 border-black rounded my-8" type="submit" value="register" @click="registerUser" />
+    </div>
     </form>
   </div>
 </template>

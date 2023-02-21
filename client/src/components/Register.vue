dc7f4aad208a5552a47fe90dd8abc5809f7c3033
redripper480 <jzoraz@gmail.com>
2023-02-21 21:38:34 +0500

    Register Page Styling

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -200,6 +200,6 @@ export default {
   transition-duration: 1s;
 }
 .curve{
-  border-radius: 30% 70% 64% 36% / 34% 47% 53% 66%  ;
+  border-radius: 83% 17% 82% 18% / 19% 80% 20% 81%   ;
 }
 </style>

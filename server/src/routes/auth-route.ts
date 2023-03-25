64160f9987c4996ccecf408ca8da7e61bdbfdcbb
SaadAhmedGit <saadahmedcronos@gmail.com>
2023-03-25 21:58:00 +0500

    Refactor

--- server/src/routes/auth-route.ts
+++ server/src/routes/auth-route.ts
@@ -1,6 +1,5 @@
 import app from "../controller/app.js";
 import User from "../models/user.js";
-import "../controller/config.js";
 
 const authRoute = async () => {
   app.post("/auth", async (req, res) => {
@@ -8,7 +7,7 @@ const authRoute = async () => {
     try {
       const authUser = await User.findOne({ username: _username });
       if (!authUser) throw new Error("Username or Password incorrect");
-      //Todo: Add password hashing auth
+      //TODO: Add password hashing auth
       res.status(200).json({
         message: "You have been logged in",
       });

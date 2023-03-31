4d165e3b10e1e1664290a75dde4a49464f3481e2
SaadAhmedGit <saadahmedcronos@gmail.com>
2023-03-31 07:13:51 +0500

    Add validation logic for authorization

---
+++ server/src/validators/auth-validator.ts
@@ -0,0 +1,41 @@
+import exp from "constants";
+
+const check = require("express-validator");
+
+const userSignupValidator = [
+  check("name").not().isEmpty().withMessage("Name is required"),
+  check("email").isEmail().withMessage("Must be a valid email address"),
+  check("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long"),
+];
+
+const userSigninValidator = [
+  check("email").isEmail().withMessage("Must be a valid email address"),
+  check("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long"),
+];
+
+const forgotPasswordValidator = [
+  check("email")
+    .not()
+    .isEmpty()
+    .isEmail()
+    .withMessage("Must be a valid email address"),
+];
+
+const resetPasswordValidator = [
+  check("newPassword")
+    .not()
+    .isEmpty()
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long"),
+];
+
+export {
+  userSignupValidator,
+  userSigninValidator,
+  forgotPasswordValidator,
+  resetPasswordValidator,
+};

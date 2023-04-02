ec9ed763d4d822c644bfb176d0fe4372e6e0d8bd
redripper480 <jzoraz@gmail.com>
2023-04-02 21:34:45 +0500

    Gradient Navbar Border

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,36 +1,44 @@
 <template>
-  <nav class="h-[70px] bg-black w-[100%] sticky border-b-2 border-purple-700">
-    <div class="flex justify-between items-center h-[100%]">
-      <div>
-        <h1
-          class="font-montserrat text-white font-semibold text-2xl text-center ml-10"
-        >
-          CODE<span class="font-montserrat text-purple-700 font-black"
-            >RATIC</span
+  <div class="bg-gradient-to-r from-purple-700 to-red-900 pb-[2px]">
+    <nav class="h-[70px] bg-black w-[100%] sticky">
+      <div class="flex justify-between items-center h-[100%]">
+        <div>
+          <h1
+            class="font-montserrat text-white font-semibold text-2xl text-center ml-10"
           >
-        </h1>
+            CODE<span class="font-montserrat text-purple-700 font-black"
+              >RATIC</span
+            >
+          </h1>
+        </div>
+        <div class="grow">
+          <ul class="flex gap-10 mr-32 justify-end">
+            <li>
+              <a class="text-white font-montserrat text-[15px]" href="">Home</a>
+            </li>
+            <li>
+              <a class="text-white font-montserrat text-[15px]" href=""
+                >Competitons</a
+              >
+            </li>
+            <li>
+              <a class="text-white font-montserrat text-[15px]" href=""
+                >Practice</a
+              >
+            </li>
+            <li>
+              <a class="text-white font-montserrat text-[15px]" href=""
+                >Profile</a
+              >
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div class="h-[40px] w-[40px] rounded-[50%] bg-white mr-10"></div>
+        </div>
       </div>
-      <div class="grow">
-        <ul class="flex gap-10 mr-32 justify-end">
-          <li>
-            <a class="text-white font-montserrat text-[15px]" href="">Home</a>
-          </li>
-          <li>
-            <a class="text-white font-montserrat text-[15px]" href="">Competitons</a>
-          </li>
-          <li>
-            <a class="text-white font-montserrat text-[15px]" href="">Practice</a>
-          </li>
-          <li>
-            <a class="text-white font-montserrat text-[15px]" href="">Profile</a>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <div class="h-[40px] w-[40px] rounded-[50%] bg-white mr-10"></div>
-      </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
 </template>
 
 <script lang="ts">

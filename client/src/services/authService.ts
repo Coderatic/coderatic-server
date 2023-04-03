2eaf8fa6dec4419f32a2a264ff7cc1f187586515
SaadAhmedGit <saadahmedcronos@gmail.com>
2023-04-03 23:15:58 +0500

    Deprecated

--- client/src/services/authService.ts
+++
@@ -1,8 +0,0 @@
-import Api from "./Api";
-
-export default {
-  authorize(username: string, password: string) {
-    console.log({ username, password });
-    return Api().post("auth", { username, password });
-  },
-};

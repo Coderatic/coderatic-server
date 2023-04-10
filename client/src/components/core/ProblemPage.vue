7f8fe50296bcae3a1450c76b220a2d789585f6e0
redripper480 <jzoraz@gmail.com>
2023-04-11 04:28:36 +0500

    Code Mirror

--- client/src/components/core/ProblemPage.vue
+++ client/src/components/core/ProblemPage.vue
@@ -26,7 +26,6 @@
         </div>
       </div>
       <hr class="border-b-1 mx-2 border-background-grey my-2" />
-      <textarea v-model="message" cols="30" rows="10"></textarea>
       <div
         class="problem-container text-white mt-8 mb-[8px] px-4 w-full overflow-y-auto h-[calc(100%-168px)]"
         v-html="markdownHtml"
@@ -38,7 +37,80 @@
       <div class="h-[20px] w-[20px] rounded-[50%] bg-gray-300 absolute"></div>
     </div>
     <div class="w-[49.8%] h-[calc(100vh-47px)] bg-background-grey-dark">
-      <div class="my-5 text-white"></div>
+      <div
+        class="w-full h-[50px] mt-3 flex flex-col justify-center px-2 relative"
+      >
+        <div class="flex justify-end gap-3">
+          <div
+            class="h-[40px] font-robotomono rounded-xl bg-background-grey text-gray-100 hover:bg-gray-600 flex justify-between items-center px-4"
+          >
+            Upload File
+          </div>
+          <div
+            v-on:click="selectLanguage"
+            class="font-robotomono h-[40px] rounded-xl bg-background-grey text-gray-100 hover:bg-gray-600 flex justify-between items-center px-4"
+          >
+            <p v-html="currentLang"></p>
+            <svg
+              class="w-4 h-4 ml-2"
+              aria-hidden="true"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M19 9l-7 7-7-7"
+              ></path>
+            </svg>
+          </div>
+          <div
+            class="w-[200px] z-[1] rounded-md absolute top-0 translate-y-[50px] bg-background-grey"
+            :class="langSelect ? 'isnline' : 'hidden'"
+          >
+            <ul>
+              <li
+                class="py-2 pl-4 text-gray-100 font-lato hover:bg-gray-600"
+                @click="(currentLang = 'JavaScript'), (langSelect = false)"
+              >
+                JavaScript
+              </li>
+              <hr class="w-full border-b-1 border-gray-600" />
+              <li
+                class="py-2 pl-4 text-gray-100 font-lato hover:bg-gray-600"
+                @click="(currentLang = 'TypeScript'), (langSelect = false)"
+              >
+                TypeScript
+              </li>
+              <hr class="w-full border-b-1 border-gray-600" />
+
+              <li
+                class="py-2 pl-4 text-gray-100 font-lato hover:bg-gray-600"
+                @click="(currentLang = 'g++ 11'), (langSelect = false)"
+              >
+                g++ 11
+              </li>
+              <hr class="w-full border-b-1 border-gray-600" />
+
+              <li
+                class="py-2 pl-4 text-gray-100 font-lato hover:bg-gray-600"
+                @click="(currentLang = 'g++ 14'), (langSelect = false)"
+              >
+                g++ 14
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="w-full h-[calc(100vh-180px)]">
+        <CodeMirror></CodeMirror>
+      </div>
+      <div class="w-full h-60px flex flex-col justify-center items-end my-3 px-2">
+        <div class="px-8 py-2 rounded-2xl bg-background-grey text-gray-100 font-robotomono">Submit</div>
+      </div>
     </div>
   </div>
 </template>
@@ -46,15 +118,35 @@
 <script lang="ts">
 import NavBar from "../global/NavBar.vue";
 import { marked } from "marked";
+import CodeMirror from "../global/CodeMirror.vue";
 export default {
   name: "ProblemPage",
+  methods: {
+    markdown() {
+      document.addEventListener("DOMContentLoaded", function () {
+        const codeElement = document.querySelector("p code") as HTMLElement;
+        if (codeElement != null) {
+          const divElement = document.createElement("div") as HTMLElement;
+          divElement.classList.add("scroll-inline-code");
+          codeElement.parentNode!.insertBefore(divElement, codeElement);
+          divElement.appendChild(codeElement);
+        }
+      });
+    },
+    selectLanguage() {
+      this.langSelect = !this.langSelect;
+    },
+  },
   data() {
     return {
-      message: "# Example",
+      message: "",
+      langSelect: false,
+      currentLang: "g++ 11",
     };
   },
   components: {
     NavBar,
+    CodeMirror,
   },
   computed: {
     markdownHtml() {
@@ -65,10 +157,15 @@ export default {
 </script>
 
 <style>
-p{
-  padding: 20px;
-  
+.scroll-inline-code {
+  outline: 2px;
+  max-width: 100%;
+  overflow-x: auto;
+  overflow-y: hidden;
+  white-space: nowrap;
 }
+
+p > div > code,
 p > code {
   padding: 5px;
   color: #ff6666;
@@ -82,28 +179,28 @@ pre {
   background-color: rgb(53, 53, 53);
   border-radius: 5px;
 }
-.problem-container > h1{
-  font-size: 2em ;
+.problem-container > h1 {
+  font-size: 2em;
   font-weight: bold;
 }
-.problem-container > h2{
-  font-size: 1.5em ;
+.problem-container > h2 {
+  font-size: 1.5em;
   font-weight: bold;
 }
-.problem-container > h3{
-  font-size: 1.17em ;
+.problem-container > h3 {
+  font-size: 1.17em;
   font-weight: bold;
 }
-.problem-container > h4{
-  font-size: 1em ;
+.problem-container > h4 {
+  font-size: 1em;
   font-weight: bold;
 }
-.problem-container > h5{
-  font-size: 0.83em ;
+.problem-container > h5 {
+  font-size: 0.83em;
   font-weight: bold;
 }
-.problem-container > h6{
-  font-size: 0.67em ;
+.problem-container > h6 {
+  font-size: 0.67em;
   font-weight: bold;
 }
 </style>

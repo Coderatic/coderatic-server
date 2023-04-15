36f4e4aa5c7314b26b96dd43949d78d6e9358d3a
SaadAhmedGit <saadahmedcronos@gmail.com>
2023-04-16 03:39:14 +0500

    Define schema

---
+++ server/src/models/sample-set-model.ts
@@ -0,0 +1,35 @@
+import mongoose, { Document } from "mongoose";
+import shortId from "shortid";
+import { IProblem } from "./problem-model.js";
+
+interface ISampleSet extends Document {
+  problem_id: mongoose.Schema.Types.ObjectId;
+  input: string;
+  output: string;
+  description: string;
+}
+
+const SampleSetSchema = new mongoose.Schema<ISampleSet>(
+  {
+    problem_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Problem",
+      required: true,
+    } as IProblem["_id"],
+    input: {
+      type: String,
+      required: true,
+    },
+    output: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+  },
+  { collection: "sampleSet" }
+);
+
+export default mongoose.model<ISampleSet>("SampleSet", SampleSetSchema);
+export { ISampleSet };

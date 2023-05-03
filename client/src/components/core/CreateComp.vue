bf44276fd435d7444cb560d755969ff19df5bc59
redripper480 <jzoraz@gmail.com>
2023-05-03 12:07:02 +0500

    create challenge

--- client/src/components/core/CreateComp.vue
+++ client/src/components/core/CreateComp.vue
@@ -72,43 +72,85 @@
                 name=""
                 id=""
               />
-              <div class="flex justify-center items-center gap-5" >
-                <Calendar v-model="start_date" dateFormat="dd/mm/yy" showIcon /> 
-                <p class="px-2">at</p> 
-                <input 
-                class="border-[2px]  bg-background-grey focus:border-purple-900 focus:outline-none py-[4px] px-[3px] text-sm text-white rounded w-[200px] my-3 font-robotomono"
-                :class="startTimeCheck ? 'border-gray-900':'border-red-500'"
-                v-model="start_time"
-                type="text" placeholder="HH:MM:SS am|pm">
+              <div class="flex justify-center items-center gap-5">
+                <Calendar v-model="start_date" dateFormat="dd/mm/yy" showIcon />
+                <p class="px-2">at</p>
+                <input
+                  class="border-[2px] bg-background-grey focus:border-purple-900 focus:outline-none py-[4px] px-[3px] text-sm text-white rounded w-[200px] my-3 font-robotomono"
+                  :class="startTimeCheck ? 'border-gray-900' : 'border-red-500'"
+                  v-model="start_time"
+                  type="text"
+                  placeholder="HH:MM:SS am|pm"
+                />
                 PKT
               </div>
-              <div class="flex justify-center items-center gap-5" >
-                <Calendar v-model="end_date" dateFormat="dd/mm/yy" showIcon /> 
-                <p class="px-2">at</p> 
-                <input 
-                class="border-[2px] border-gray-900 bg-background-grey focus:border-purple-900 focus:outline-none py-[4px] px-[3px] text-sm text-white rounded w-[200px] my-3 font-robotomono"
-                :class="endTimeCheck ? 'border-gray-900':'border-red-500'"
-                v-model="end_time"
-                type="text" placeholder="HH:MM:SS am|pm"> 
+              <div class="flex justify-center items-center gap-5">
+                <Calendar v-model="end_date" dateFormat="dd/mm/yy" showIcon />
+                <p class="px-2">at</p>
+                <input
+                  class="border-[2px] border-gray-900 bg-background-grey focus:border-purple-900 focus:outline-none py-[4px] px-[3px] text-sm text-white rounded w-[200px] my-3 font-robotomono"
+                  :class="endTimeCheck ? 'border-gray-900' : 'border-red-500'"
+                  v-model="end_time"
+                  type="text"
+                  placeholder="HH:MM:SS am|pm"
+                />
                 PKT
               </div>
-              
             </div>
           </div>
         </div>
-        <div class="flex flex-col items-start w-full gap-5"
-          v-if="currentTab === 'challenges'">
+        <div
+          class="flex flex-col items-start w-full gap-5"
+          v-if="currentTab === 'challenges'"
+        >
           <h1 class="text-3xl">Contest Challenges</h1>
           <p class="mb-5 text-gray-500">
-            Please provide the challenges you want to add in your competition here.
+            Please provide the challenges you want to add in your competition
+            here.
           </p>
-          <div class="w-full flex flex-col justify-center items-start" v-if="addchallenge">
-            
+          <div
+            class="w-full flex flex-col justify-center items-start"
+            v-if="addchallenge"
+          >
+            <div class="flex justify-center items-center gap-10">
+              <h1 class="w-[200px]">Problem Title</h1>
+
+              <input
+                class="border-[2px] bg-background-grey focus:border-purple-900 focus:outline-none py-[4px] px-[3px] text-sm text-white rounded w-[200px] my-3 font-robotomono"
+                type="text"
+              />
+            </div>
+            <div class="w-full flex justify-start items-center gap-10">
+              <h1 class="w-[200px]">Problem Statement</h1>
+
+              <textarea
+                class="border-[2px] bg-background-grey focus:border-purple-900 focus:outline-none py-[4px] px-[3px] text-sm text-white rounded w-[200px] my-3 font-robotomono"
+                
+              ></textarea>
+            </div>
+            <div class="w-full flex justify-start items-center gap-10">
+              <h1 class="w-[200px]">Input Format</h1>
+
+              <textarea
+                class="border-[2px] bg-background-grey focus:border-purple-900 focus:outline-none py-[4px] px-[3px] text-sm text-white rounded w-[200px] my-3 font-robotomono"
+                
+              ></textarea>
+            </div>
+            <div class="w-full flex justify-start items-center gap-10">
+              <h1 class="w-[200px]">Output Format</h1>
+
+              <textarea
+                class="border-[2px] bg-background-grey focus:border-purple-900 focus:outline-none py-[4px] px-[3px] text-sm text-white rounded w-[200px] my-3 font-robotomono"
+                
+              ></textarea>
+            </div>
           </div>
-          <div class="border-[0.5px] select-none cursor-pointer rounded hover:bg-gray-800 px-2 py-2"
-          v-on:click="addchallenge = !addchallenge"
-          >ADD CHALLENGE</div>
-          
+          <div
+            class="border-[0.5px] select-none cursor-pointer rounded hover:bg-gray-800 px-2 py-2"
+            v-on:click="addchallenge = !addchallenge"
+          >
+            ADD CHALLENGE
+          </div>
         </div>
       </div>
     </div>
@@ -126,24 +168,23 @@ export default {
   data() {
     return {
       currentTab: "detail",
-      start_date:"",
-      start_time:"",
-      end_date:"",
-      end_time:"",
-      addchallenge:false
-      
+      start_date: "",
+      start_time: "",
+      end_date: "",
+      end_time: "",
+      addchallenge: false,
     };
   },
-  computed:{
-    startTimeCheck(){
-      var timeregex = /^([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9] (am|pm)$/ ;
-      return this.start_time === '' || timeregex.test(this.start_time);
+  computed: {
+    startTimeCheck() {
+      var timeregex = /^([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9] (am|pm)$/;
+      return this.start_time === "" || timeregex.test(this.start_time);
     },
-    endTimeCheck(){
-      var timeregex = /^([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9] (am|pm)$/ ;
-      return this.end_time === '' || timeregex.test(this.end_time);
-    }
-  }
+    endTimeCheck() {
+      var timeregex = /^([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9] (am|pm)$/;
+      return this.end_time === "" || timeregex.test(this.end_time);
+    },
+  },
 };
 </script>
 

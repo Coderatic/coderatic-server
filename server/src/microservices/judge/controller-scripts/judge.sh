477599706f5b8bb9bb3d2577b171044736f23058
SaadAhmedGit <saadahmedcronos@gmail.com>
2023-05-14 17:34:22 +0500

    Add apparmor instead of docker

--- server/src/microservices/judge/controller-scripts/judge.sh
+++ server/src/microservices/judge/controller-scripts/judge.sh
@@ -6,9 +6,7 @@ lang="$3"
 in_file_name="$4"
 out_file_name="$5"
 
-host_shared_path="$(pwd)/../cache/processed"
-bin_path="/processed/bins"
-scripts_path="/processed/scripts"
+proc_path="../cache/processed"
 test_sets="../cache/test_sets"
 
 in_file="$test_sets"/"$problem"/input/"$in_file_name"
@@ -20,24 +18,31 @@ mem_limit="$6"
 time_limit="$7"
 
 case "$lang" in
-cpp | c | rust)
-	timeout "$time_limit"s docker run --rm -v "$host_shared_path/bins":"$bin_path" --network none --security-opt apparmor=docker-default --cap-drop=SETFCAP --read-only --runtime=runsc --memory="$mem_limit"m -i -e BIN="$exec" bin_img <"$in_file" | ("$THE_JUDGE" "$out_file")
+cpp | c | rs)
+	(ulimit -t "$time_limit" -v "$mem_limit"; aa-exec -p bin_jail "$proc_path/bins/$exec")  <"$in_file" | "$THE_JUDGE" "$out_file"
 	pipe_exit_codes=("${PIPESTATUS[@]}")
-	docker_exit_code="${pipe_exit_codes[0]}"
+	program_exit_code="${pipe_exit_codes[0]}"
 	judge_exit_code="${pipe_exit_codes[1]}"
 
 	exit_code=$judge_exit_code
-	if [ "$docker_exit_code" -ne 0 ]; then
-		exit_code=$docker_exit_code
+	if [ "$program_exit_code" -ne 0 ]; then
+		exit_code=$program_exit_code
 	fi
 	;;
 java)
 	(docker run --rm -v "$host_shared_path/bins":"$bin_path" --runtime=runsc -i -e BIN="$exec" java_img <"$in_file") | ("$THE_JUDGE" "$out_file")
 	exit_code=$?
 	;;
-py3)
-	(docker run --rm -v "$host_shared_path/scripts":"$scripts_path" --runtime=runsc -i -e BIN="$exec" py3_img <"$in_file") | ("$THE_JUDGE" "$out_file")
-	exit_code=$?
+py)
+	(ulimit -t "$time_limit" -v "$mem_limit"; aa-exec -p py3_jail python3 "$proc_path/scripts/$exec.$lang")  < "$in_file" | "$THE_JUDGE" "$out_file"
+	pipe_exit_codes=("${PIPESTATUS[@]}")
+	program_exit_code="${pipe_exit_codes[0]}"
+	judge_exit_code="${pipe_exit_codes[1]}"
+
+	exit_code=$judge_exit_code
+	if [ "$program_exit_code" -ne 0 ]; then
+		exit_code=$program_exit_code
+	fi
 	;;
 *)
 	echo "Language not supported"

d14a847b874559da72b30272e0dcd7e3a50bbe5d
Saad Ahmed <saadahmedcronos@gmail.com>
2023-07-24 13:17:56 +0500

    Set cookie with the token and use run-time envs from config

--- server/src/controller/auth-controller.ts
+++ server/src/controller/auth-controller.ts
@@ -2,310 +2,329 @@ import User from "../models/user-model.js";
 import jwt from "jsonwebtoken";
 import _ from "lodash";
 import { expressjwt as expressJwt } from "express-jwt";
-import dotenv from "dotenv";
-dotenv.config();
 
 // sendgrid
 import sgMail from "@sendgrid/mail";
-import { env } from "process";
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+import {
+	CLIENT_URI,
+	TOKEN_AGE,
+	COOKIE_AGE,
+	COOKIE_SECURE,
+	COOKIE_SAME_SITE,
+	COOKIE_HTTP_ONLY,
+} from "../configs/runtime-envs.config.js";
+
 const preSignUp = async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    const user = await User.findOne({
-      $or: [{ username: username }, { email: email }],
-    });
+	try {
+		const { username, email, password } = req.body;
+		const user = await User.findOne({
+			$or: [{ username: username }, { email: email }],
+		});
 
-    // If user is found, return error
-    if (user) {
-      return res.status(400).json({
-        error: "Username or email is already taken",
-      });
-    }
+		// If user is found, return error
+		if (user) {
+			return res.status(400).json({
+				error: "Username or email is already taken",
+			});
+		}
 
-    const token = jwt.sign(
-      { username, email, password },
-      process.env.JWT_ACCOUNT_ACTIVATION,
-      {
-        //TODO: Change it to 5m before production
-        expiresIn: "24h",
-      }
-    );
+		const token = jwt.sign(
+			{ username, email, password },
+			process.env.JWT_ACCOUNT_ACTIVATION,
+			{
+				expiresIn: process.env.NODE_ENV === "production" ? "10m" : "1d",
+			}
+		);
 
-    const emailData = {
-      to: email,
-      from: process.env.EMAIL_FROM,
-      subject: "Welcome to Coderatic! - Account Activation Link",
-      html: `
+		const emailData = {
+			to: email,
+			from: process.env.EMAIL_FROM,
+			subject: "Welcome to Coderatic! - Account Activation Link",
+			html: `
 	  <h4>Please use the following link to activate your account:</h4>
-	  <p>http://localhost:3000/auth/account/activate/${token}</p>
+	  <p>${CLIENT_URI}/auth/account/activate/${token}</p>
 
 	  <hr/>
 	  <p>This email may contain sensitive information</p>
-	  <a href="${
-      process.env.PROD_CLIENT_URL || process.env.DEV_CLIENT_URL
-    }">https://coderatic.net</a>`,
-    };
+	  <a href="${CLIENT_URI}">https://coderatic.net</a>`,
+		};
 
-    sgMail.send(emailData).then((sent) => {
-      return res.json({
-        message: `
+		sgMail.send(emailData).then((sent) => {
+			return res.json({
+				message: `
       Email has been sent to ${email}. 
       Follow the instructions to activate your account.`,
-      });
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Internal Server Error",
-    });
-  }
+			});
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: "Internal Server Error",
+		});
+	}
 };
 
 const signup = async (req, res) => {
-  try {
-    const { username, email, password } = req.user;
+	try {
+		const { username, email, password } = req.user;
 
-    const user = new User({
-      username: username,
-      email: email,
-      password: password,
-      authMethod: "local",
-    });
-    await user.save();
-    return res.json({
-      message: "Signup sucessful! Please sign in.",
-    });
-  } catch (err) {
-    return res.status(401).json({
-      error: err,
-    });
-  }
+		const user = new User({
+			username: username,
+			email: email,
+			password: password,
+			authMethod: "local",
+		});
+		await user.save();
+		return res.json({
+			message: "Signup sucessful! Please sign in.",
+		});
+	} catch (err) {
+		return res.status(401).json({
+			error: err,
+		});
+	}
 };
-import { Response } from "express";
-const login = (req, res) => {
-  try {
-    const { username, password } = req.body;
-    // check if user exist
-    User.findOne({ username: username })
-      .exec()
-      .then((user) => {
-        if (!user) {
-          return res.status(401).json({
-            message: "This username does not exist. Please signup.",
-          });
-        }
+const login = async (req, res) => {
+	try {
+		const { username, password } = req.body;
+		const user = await User.findOne({ username: username });
+		if (!user) {
+			return res.status(401).json({
+				message: "This username does not exist. Please signup.",
+			});
+		}
+		if (!user.authenticate(password)) {
+			return res.status(401).json({
+				message: "Username and password do not match.",
+			});
+		}
 
-        // authenticate
-        if (!user.authenticate(password)) {
-          return res.status(401).json({
-            message: "Username and password do not match.",
-          });
-        }
+		const { _id, email, first_name, last_name, role } = user;
+		const userData = {
+			_id,
+			username,
+			email,
+			first_name,
+			last_name,
+			role,
+		};
 
-        // generate a token and send to client
-        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
-          expiresIn: "1h",
-        });
+		const token = jwt.sign({ _id: userData._id, username: userData.username,
+      email: userData.email,
+      first_name: userData.first_name,
+      last_name: userData.last_name,
+      role: userData.role
+    },
+      process.env.JWT_SECRET, {
+		  expiresIn: TOKEN_AGE,
+		});
+		res.cookie("token", token, {
+			maxAge: COOKIE_AGE,
+			httpOnly: COOKIE_HTTP_ONLY,
+			secure: COOKIE_SECURE,
+			sameSite: COOKIE_SAME_SITE,
+		});
 
-        const { _id, username, first_name, last_name, email, role } = user;
-        return res.json({
-          token,
-          user: { _id, username, first_name, last_name, email, role },
-        });
-      });
-  } catch (err) {
-    console.log(err);
-  }
+		return res.json({
+			token,
+			user: userData,
+		});
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 const signout = (req, res) => {
-  try {
-    res.clearCookie("token");
-    res.json({
-      message: "Signout success",
-    });
-  } catch (err) {
-    console.log(err);
-  }
+	try {
+		res.clearCookie("token");
+		res.json({
+			message: "Signout success",
+		});
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 const requireSignin = expressJwt({
-  secret: process.env.JWT_SECRET,
-  algorithms: ["HS256"],
+	secret: process.env.JWT_SECRET,
+	algorithms: ["HS256"],
 });
 
 const authMiddleWare = (req, res, next) => {
-  try {
-    const authUserId = req.user._id;
-    // based on the user id, query the database and find user
-    // then make it available in the request.profile object
-    User.findById({ _id: authUserId })
-      .exec()
-      .then((user) => {
-        if (!user) {
-          return res.status(400).json({
-            error: "User not found",
-          });
-        }
-        req.profile = user;
-        // execute callback function so it can be used as a middleware
-        next();
-      });
-  } catch (err) {
-    console.log(err);
-  }
+	try {
+		const authUserId = req.user._id;
+		// based on the user id, query the database and find user
+		// then make it available in the request.profile object
+		User.findById({ _id: authUserId })
+			.exec()
+			.then((user) => {
+				if (!user) {
+					return res.status(400).json({
+						error: "User not found",
+					});
+				}
+				req.profile = user;
+				// execute callback function so it can be used as a middleware
+				next();
+			});
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 const adminMiddleWare = (req, res, next) => {
-  try {
-    const adminUserId = req.user._id;
-    // based on the user id, query the database and find user
-    // then make it available in the request.profile object
-    User.findById({ _id: adminUserId })
-      .exec()
-      .then((user) => {
-        if (!user) {
-          return res.status(400).json({
-            error: "User not found",
-          });
-        }
-        // check if admin
-        if (user.role !== 1) {
-          return res.status(400).json({
-            error: "Admin resource. Access denied",
-          });
-        }
+	try {
+		const adminUserId = req.user._id;
+		// based on the user id, query the database and find user
+		// then make it available in the request.profile object
+		User.findById({ _id: adminUserId })
+			.exec()
+			.then((user) => {
+				if (!user) {
+					return res.status(400).json({
+						error: "User not found",
+					});
+				}
+				// check if admin
+				if (user.role !== 1) {
+					return res.status(400).json({
+						error: "Admin resource. Access denied",
+					});
+				}
 
-        req.profile = user;
-        next();
-      });
-  } catch (err) {
-    console.log(err);
-  }
+				req.profile = user;
+				next();
+			});
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 const forgotPassword = async (req, res) => {
-  try {
-    const { email } = req.body;
+	try {
+		const { email } = req.body;
 
-    User.findOne({ email }, (err, user) => {
-      if (err || !user) {
-        return res.status(401).json({
-          error: "User with that email does not exist",
-        });
-      }
+		User.findOne({ email }, (err, user) => {
+			if (err || !user) {
+				return res.status(401).json({
+					error: "User with that email does not exist",
+				});
+			}
 
-      const token = jwt.sign(
-        { _id: user._id },
-        process.env.JWT_RESET_PASSWORD,
-        {
-          expiresIn: "10m",
-        }
-      );
+			const token = jwt.sign(
+				{ _id: user._id },
+				process.env.JWT_RESET_PASSWORD,
+				{
+					expiresIn: "10m",
+				}
+			);
 
-      const emailData = {
-        to: email,
-        from: process.env.EMAIL_FROM,
-        subject: "Password reset link",
-        html: `
+			const emailData = {
+				to: email,
+				from: process.env.EMAIL_FROM,
+				subject: "Password reset link",
+				html: `
           <h4>Please use the following link to reset your password:</h4>
           <p>${
-            process.env.PROD_CLIENT_URL || process.env.DEV_CLIENT_URL
-          }/auth/password/reset/${token}</p>
+				process.env.PROD_CLIENT_URL || process.env.DEV_CLIENT_URL
+			}/auth/password/reset/${token}</p>
   
           <hr/>
           <p>This email may contain sensitive information</p>
           <a href="https://coderatic.net">https://coderatic.net</a>
       `,
-      };
+			};
 
-      return user.updateOne({ resetPasswordLink: token }, (err, success) => {
-        if (err) {
-          return res.status(400).json({
-            error: err,
-          });
-        } else {
-          sgMail.send(emailData).then((sent) =>
-            res.json({
-              message: `
+			return user.updateOne(
+				{ resetPasswordLink: token },
+				(err, success) => {
+					if (err) {
+						return res.status(400).json({
+							error: err,
+						});
+					} else {
+						sgMail.send(emailData).then((sent) =>
+							res.json({
+								message: `
                 Email has been sent to ${email}. 
                 Follow the instructions to reset your password. 
                 Link expires in 10min
                 `,
-            })
-          );
-        }
-      });
-    });
-  } catch (err) {
-    console.log(err);
-  }
+							})
+						);
+					}
+				}
+			);
+		});
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 const resetPassword = async (req, res) => {
-  try {
-    const { resetPasswordLink, newPassword } = req.body;
+	try {
+		const { resetPasswordLink, newPassword } = req.body;
 
-    // check if you have the reset password
-    if (resetPasswordLink) {
-      jwt.verify(
-        // verify if the token has expired
-        resetPasswordLink,
-        process.env.JWT_RESET_PASSWORD,
-        (err, decoded) => {
-          if (err) {
-            return res.status(401).json({
-              error: "Expired link. Try again",
-            });
-          }
+		// check if you have the reset password
+		if (resetPasswordLink) {
+			jwt.verify(
+				// verify if the token has expired
+				resetPasswordLink,
+				process.env.JWT_RESET_PASSWORD,
+				(err, decoded) => {
+					if (err) {
+						return res.status(401).json({
+							error: "Expired link. Try again",
+						});
+					}
 
-          // find the user based on reset password link
-          User.findOne({ resetPasswordLink }, (err, user) => {
-            if (err || !user) {
-              return res.status(401).json({
-                error: "Something went wrong. Try later",
-              });
-            }
+					// find the user based on reset password link
+					User.findOne({ resetPasswordLink }, (err, user) => {
+						if (err || !user) {
+							return res.status(401).json({
+								error: "Something went wrong. Try later",
+							});
+						}
 
-            // update user fields
-            const updatedFields = {
-              password: newPassword,
-              resetPasswordLink: "",
-            };
+						// update user fields
+						const updatedFields = {
+							password: newPassword,
+							resetPasswordLink: "",
+						};
 
-            user = _.extend(user, updatedFields); // update fields that have changed
+						user = _.extend(user, updatedFields); // update fields that have changed
 
-            // save user with updated information
-            user.save((err, result) => {
-              if (err) {
-                return res.status(401).json({
-                  error: err,
-                });
-              }
+						// save user with updated information
+						user.save((err, result) => {
+							if (err) {
+								return res.status(401).json({
+									error: err,
+								});
+							}
 
-              res.json({
-                message: `Great! Now you can login with your new password`,
-              });
-            });
-          });
-        }
-      );
-    }
-  } catch (err) {
-    console.log(err);
-  }
+							res.json({
+								message: `Great! Now you can login with your new password`,
+							});
+						});
+					});
+				}
+			);
+		}
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 export {
-  preSignUp,
-  signup,
-  login,
-  signout,
-  requireSignin,
-  authMiddleWare,
-  adminMiddleWare,
-  forgotPassword,
-  resetPassword,
+	preSignUp,
+	signup,
+	login,
+	signout,
+	requireSignin,
+	authMiddleWare,
+	adminMiddleWare,
+	forgotPassword,
+	resetPassword,
 };

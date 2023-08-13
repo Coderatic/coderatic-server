26d8578d501c38335d8b7766b84680f2d407da87
Saad Ahmed <saadahmedcronos@gmail.com>
2023-08-13 19:53:37 +0500

    Add slug and hold all related submissions in model

--- server/src/models/contest-model.ts
+++ server/src/models/contest-model.ts
@@ -1,63 +1,78 @@
 import mongoose, { Document } from "mongoose";
-import HiddenTest from "./hidden-test-model.js";
 import shortId from "shortid";
 
 interface IContest extends Document {
-  name: string;
-  short_id: string;
-  starting_time: Date;
-  ending_time: Date;
-  participants: [
-    {
-      user_id: {
-        type: mongoose.Schema.Types.ObjectId;
-        ref: "User";
-      };
-      score: number;
-      penalty: number;
-    }
-  ];
+	name: string;
+	slug: string;
+	starting_time: Date;
+	ending_time: Date;
+	participants: [
+		{
+			user_id: {
+				type: mongoose.Schema.Types.ObjectId;
+				ref: "User";
+			};
+			score: number;
+			penalty: number;
+		}
+	];
+	submissions: [
+		{
+			submission_id: {
+				type: mongoose.Schema.Types.ObjectId;
+				ref: "Submission";
+			};
+		}
+	];
 }
 
 const ContestSchema = new mongoose.Schema<IContest>(
-  {
-    name: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    short_id: {
-      type: String,
-      unique: true,
-      default: shortId.generate,
-    },
-    starting_time: {
-      type: Date,
-      required: true,
-    },
-    ending_time: {
-      type: Date,
-      required: true,
-    },
-    participants: [
-      {
-        user_id: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "User",
-        },
-        score: {
-          type: Number,
-          default: 0,
-        },
-        penalty: {
-          type: Number,
-          default: 0,
-        },
-      },
-    ],
-  },
+	{
+		name: {
+			type: String,
+			trim: true,
+			required: true,
+		},
+		slug: {
+			type: String,
+			unique: true,
+			default: function () {
+				return this.name.toLowerCase().replace(/ /g, "-");
+			},
+		},
+		starting_time: {
+			type: Date,
+			required: true,
+		},
+		ending_time: {
+			type: Date,
+			required: true,
+		},
+		participants: [
+			{
+				user_id: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: "User",
+				},
+				score: {
+					type: Number,
+					default: 0,
+				},
+				penalty: {
+					type: Number,
+					default: 0,
+				},
+			},
+		],
+		submissions: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Submission",
+			},
+		],
+	},
 
-  { collection: "contest", timestamps: true }
+	{ collection: "contest", timestamps: true }
 );
 
 export default mongoose.model<IContest>("Contest", ContestSchema);

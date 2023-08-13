f87a0fb951143b217bb0189246de012456098f82
Saad Ahmed <saadahmedcronos@gmail.com>
2023-08-13 19:49:19 +0500

    Move all pre-processing related utils and processing code away from queueing logic

--- server/src/microservices/judge/utils.ts
+++ server/src/microservices/judge/utils.ts
@@ -1,132 +1,202 @@
 import fs from "fs";
+import path from "path";
 import { exec } from "child_process";
 import { v4 as uuidv4 } from "uuid";
 
 //Types
-import { verdict } from "./judge-queue.types.js";
+import { TestResult, Verdict } from "./judge-queue.types.js";
 import { JudgeJob } from "../../controller/types/submission-controller.types.js";
 
-const handle_exit_code = (exit_code: Number): string => {
-  switch (exit_code) {
-    case 0:
-      return "AC";
-    case 1:
-      return "CE";
-    case 2:
-      return "WA";
-    case 5:
-      return "IE";
-    case 137:
-      return "TLE";
-    case 139:
-      return "MLE";
-    default:
-      console.log("Unknown exit code: ", exit_code);
-      return "RE";
-  }
+//Directory config
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { check } from "express-validator";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Constants
+const CWD = __dirname;
+const JUDGE_WD = path.join(CWD, "controller-scripts");
+
+const handle_exit_code = (exit_code: Number): Verdict => {
+	switch (exit_code) {
+		case 0:
+			return "Accepted";
+		case 2:
+			return "Wrong Answer";
+		case 5:
+			return "IE";
+		case 137:
+			return "TLE";
+		case 139:
+			return "MLE";
+		default:
+			//TODO: Remove this log before hitting production.
+			console.log("Unknown exit code: ", exit_code);
+			return "RE";
+	}
 };
 
 const escapeSrc = (source_code: string): string => {
-  source_code.replace(/[\\n\\t\\r\\]/g, (match) => {
-    switch (match) {
-      case "\\n":
-        return "\\n";
-      case "\\t":
-        return "\\t";
-      case "\\r":
-        return "\\r";
-      case "\\\\":
-        return "\\\\";
-      case '"':
-        return '\\"';
-      default:
-        return match;
-    }
-  });
-  return source_code;
+	return source_code.replace(/[\n\t\r\\"]/g, (match) => `${match}`);
 };
 
 const cleanup = async (file_paths: string[]) => {
-  for (let i = 0; i < file_paths.length; i++) {
-    const file_path = file_paths[i];
-    if (file_path) {
-      fs.unlink(file_path, (err) => {
-        if (err) console.log(err);
-      });
-    }
-  }
+	for (let i = 0; i < file_paths.length; i++) {
+		const file_path = file_paths[i];
+		if (file_path) {
+			fs.unlink(file_path, (err) => {
+				if (err) console.log(err);
+			});
+		}
+	}
 };
 
-const runCommand = async (
-  command: string,
-  workingDir: string
-): Promise<any> => {
-  return new Promise((resolve, reject) => {
-    const process = exec(
-      command,
-      { cwd: workingDir },
-      (error, stdout, stderr) => {
-        if (error) {
-          reject(process);
-        } else {
-          resolve({ process, stdout, stderr });
-        }
-      }
-    );
-  });
+const runCommand = (command: string, workingDir: string): Promise<any> => {
+	return new Promise((resolve, reject) => {
+		const process = exec(
+			command,
+			{ cwd: workingDir },
+			(error, stdout, stderr) => {
+				if (error) {
+					reject(process);
+				} else {
+					resolve({ process, stdout, stderr });
+				}
+			}
+		);
+	});
 };
 
-const createTestFiles = async (raw_input: string, raw_output: string) => {
-  const input_file = uuidv4() + ".txt";
-  const output_file = uuidv4() + ".txt";
-  fs.writeFileSync(input_file, raw_input, { flag: "w" });
-  fs.writeFileSync(output_file, raw_output, { flag: "w" });
-  return { input_file, output_file };
+const createTestDirectories = async (directoryPath: string) => {
+	if (!fs.existsSync(directoryPath)) {
+		fs.mkdirSync(`${directoryPath}/input/`, { recursive: true });
+		fs.mkdirSync(`${directoryPath}/output/`, { recursive: true });
+	}
+};
+
+const createTestFiles = async (
+	input_filename: string,
+	output_filename: string,
+	raw_input: string,
+	raw_output: string,
+	directoryPath: string
+) => {
+	fs.writeFileSync(`${directoryPath}/input/${input_filename}`, raw_input, {
+		flag: "w",
+	});
+	fs.writeFileSync(`${directoryPath}/output/${output_filename}`, raw_output, {
+		flag: "w",
+	});
 };
 
 const startJudging = async (
-  workingDir: string,
-  testCases: any[],
-  job_data: JudgeJob,
-  testIsRaw: boolean
-): Promise<verdict[]> => {
-  const verdicts: verdict[] = [];
-  const { problem_id, lang, file_name } = job_data;
-  for (let i = 0; i < testCases.length; i++) {
-    const tc = testCases[i];
-    let input_file_path = "";
-    let output_file_path = "";
-    if (testIsRaw) {
-      const { input_file, output_file } = await createTestFiles(
-        tc.test_input,
-        tc.test_output
-      );
-      input_file_path = input_file;
-      output_file_path = output_file;
-    }
-    const judge_script = `./judge.sh ${problem_id} ${file_name} ${
-      lang.extension
-    } ${input_file_path} ${output_file_path} ${Number(tc.mem_lim) * 1024} ${
-      tc.time_lim
-    }`;
+	file_name: string,
+	testCases: any[],
+	job_data: JudgeJob,
+	testType: string
+): Promise<TestResult[]> => {
+	const verdicts: TestResult[] = [];
+	const { slug, lang, time_lim, mem_lim } = job_data.problemData;
+	for (let i = 0; i < testCases.length; i++) {
+		const tc = testCases[i];
+		let input_filename = tc?.input_file_path;
+		let output_filename = tc?.output_file_path;
+		if (testType == "sample") {
+			const testsDirectoryPath = path.join(
+				CWD,
+				`./cache/test_sets/${slug}`
+			);
+			await createTestDirectories(testsDirectoryPath);
+			input_filename = `sample-${i + 1}.in`;
+			output_filename = `sample-${i + 1}.out`;
+			await createTestFiles(
+				input_filename,
+				output_filename,
+				tc.test_input,
+				tc.test_output,
+				testsDirectoryPath
+			);
+		}
+		const judge_script = `./judge.sh ${slug} ${file_name} ${
+			lang.extension
+		} ${input_filename} ${output_filename} ${
+			Number(tc.mem_lim ?? mem_lim) * 1024
+		} ${tc.time_lim ?? time_lim}`;
 
-    const verd: verdict = {
-      judge_verdict: "IE",
-      test_case_type: "hidden",
-    };
-    try {
-      const result = await runCommand(judge_script, workingDir);
-      verd.judge_verdict = handle_exit_code(result.stdout);
-    } catch (err) {
-      if (err.exitCode !== undefined) {
-        verd.judge_verdict = handle_exit_code(Number(err.exitCode));
-      }
-    } finally {
-      verdicts.push(verd);
-      cleanup([input_file_path, output_file_path]);
-    }
-  }
-  return verdicts;
+		let tcResult: TestResult = {
+			verdict: "IE",
+		};
+		try {
+			const judge_script_result = await runCommand(
+				judge_script,
+				JUDGE_WD
+			);
+			const judge_output = JSON.parse(
+				judge_script_result.stdout as string
+			);
+
+			// TODO: Refactor this
+			//Convert strings entries to numbers
+			const {
+				cpu_time: cpu_time,
+				memory: memory,
+				program_exit_code: programExitCode,
+				checker_exit_code: checkerExitCode,
+			} = judge_output;
+
+			const integerJudgeOutputEntries = {
+				cpu_time: parseInt(cpu_time),
+				memory: parseInt(memory),
+				program_exit_code: parseInt(programExitCode),
+				checker_exit_code: parseInt(checkerExitCode),
+			};
+
+			if (integerJudgeOutputEntries.program_exit_code != 0) {
+				throw "Runtime Error";
+			}
+			const verdict = judge_output.checker_output.verdict;
+			tcResult = { ...tcResult, verdict, cpu_time, memory };
+		} catch (err) {
+			if (err === "Runtime Error") {
+				tcResult.verdict = "RE";
+			} else {
+				console.log("Judge Error: ", err);
+				tcResult.verdict = "IE";
+			}
+		} finally {
+			verdicts.push(tcResult);
+			if (tcResult.verdict !== "Accepted") break;
+		}
+	}
+	return verdicts;
 };
 
-export { startJudging, cleanup, handle_exit_code, runCommand, escapeSrc };
+const aggregateResults = (
+	sample_test_results: TestResult[],
+	hidden_test_results: TestResult[]
+): { verdict: Verdict; cpu_time: number; memory: number } => {
+	const allTestResults = [...sample_test_results, ...hidden_test_results];
+	let final_verdict = allTestResults.at(-1).verdict;
+	let max_cpu_time = 0;
+	let peak_memory = 0;
+	for (let i = 0; i < allTestResults.length; i++) {
+		max_cpu_time = Math.max(max_cpu_time, allTestResults[i].cpu_time);
+		peak_memory = Math.max(peak_memory, allTestResults[i].memory);
+	}
+	max_cpu_time /= allTestResults.length;
+	return {
+		verdict: final_verdict,
+		cpu_time: max_cpu_time,
+		memory: peak_memory,
+	};
+};
+
+export {
+	startJudging,
+	cleanup,
+	handle_exit_code,
+	runCommand,
+	escapeSrc,
+	aggregateResults,
+};

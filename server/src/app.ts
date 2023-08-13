ae285477d8228ca2280102dcda933d7c255ec7de
Saad Ahmed <saadahmedcronos@gmail.com>
2023-08-13 19:40:57 +0500

    Add queue processing resume upon restart

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,6 +5,9 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import passportSetup from "./configs/passport-setup.js";
 
+//Microservices
+import JudgeQueue from "./microservices/judge/judge-queue.js";
+
 //Configs
 import { connectDB } from "./configs/db-setup.js";
 connectDB();
@@ -16,48 +19,52 @@ import submissionRoutes from "./routes/submission-routes.js";
 import problemRoutes from "./routes/problem-routes.js";
 
 const app = Express();
-import http from "http";
-
-http.createServer(app);
 
 import dotenv from "dotenv";
 dotenv.config();
 
-// Cleaner env variables
-const NODE_ENV = process.env.NODE_ENV;
-const CLIENT_URL =
-  NODE_ENV == "production"
-    ? process.env.PROD_CLIENT_URL
-    : process.env.DEV_CLIENT_URL;
+import { CLIENT_URI, SERVER_URI } from "./configs/runtime-envs.config.js";
 
 // middlewares
-app.use(morgan(NODE_ENV === "production" ? "tiny" : "dev"));
+app.use(morgan(process.env.NODE_ENV === "production" ? "tiny" : "dev"));
 app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({
-    limit: "50mb",
-    extended: true,
-    parameterLimit: 50000,
-  })
+	bodyParser.urlencoded({
+		limit: "50mb",
+		extended: true,
+		parameterLimit: 50000,
+	})
 );
 app.use(cookieParser());
 app.use(passportSetup.initialize());
 
 // Set credentials to true
 const corsOptions = {
-  origin: CLIENT_URL,
-  credentials: true,
-  optionSuccessStatus: 200,
+	origin: CLIENT_URI,
+	credentials: true,
+	optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 
 // routes middlewares
-app.use("/api/auth", authRoutes);
+app.use("/api/auth/", authRoutes);
 app.use("/api/oauth", OAuthRoutes);
 app.use("/api/problems", problemRoutes);
 app.use("/api/submission", submissionRoutes);
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server is up and running at ${CLIENT_URL}.`);
+app.listen(PORT, async () => {
+	console.log(`Server is up and running at ${SERVER_URI}.`);
+	const awaitedJobs =
+		(await JudgeQueue.getFailedCount()) +
+		(await JudgeQueue.getWaitingCount()) +
+		(await JudgeQueue.getActiveCount());
+	if (awaitedJobs > 0) {
+		try {
+			await JudgeQueue.resume(true);
+			console.log("Queue processing resumed.");
+		} catch (error) {
+			console.error("Error resuming queue processing:", error);
+		}
+	}
 });

48d73beeb4180d3c8123e8d59dd7afaa2a82d7b3
Saad Ahmed <saadahmedcronos@gmail.com>
2023-08-13 19:56:02 +0500

    Remove time and mem limits along with problem reference

--- server/src/models/sample-test-model.ts
+++ server/src/models/sample-test-model.ts
@@ -1,45 +1,30 @@
 import mongoose, { Document } from "mongoose";
-import { IProblem } from "./problem-model.js";
-
 interface ISampleTest extends Document {
-  problem_id: mongoose.Schema.Types.ObjectId;
-  test_input: string;
-  test_output: string;
-  explanation?: string;
-  time_lim: number;
-  mem_lim: number;
+	test_input: string;
+	test_output: string;
+	explanation?: string;
 }
 
-const HiddenTestSchema = new mongoose.Schema<ISampleTest>(
-  {
-    problem_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Problem",
-      required: true,
-    } as IProblem["_id"],
-    test_input: {
-      type: String,
-      required: true,
-    },
-    test_output: {
-      type: String,
-      required: true,
-    },
-    explanation: {
-      type: String,
-      required: false,
-    },
-    time_lim: {
-      type: Number,
-      required: true,
-    },
-    mem_lim: {
-      type: Number,
-      required: true,
-    },
-  },
-  { collection: "sampleTest" }
+const SampleTestSchema = new mongoose.Schema<ISampleTest>(
+	{
+		test_input: {
+			type: String,
+			required: true,
+			maxlength: 1000,
+		},
+		test_output: {
+			type: String,
+			required: true,
+			maxlength: 1000,
+		},
+		explanation: {
+			type: String,
+			required: false,
+			maxlength: 500,
+		},
+	},
+	{ collection: "sampleTest" }
 );
 
-export default mongoose.model<ISampleTest>("SampleTest", HiddenTestSchema);
+export default mongoose.model<ISampleTest>("SampleTest", SampleTestSchema);
 export { ISampleTest };

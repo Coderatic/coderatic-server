344fb9f39397f3c53f04875ea8b09b2b68f4499c
Saad Ahmed <saadahmedcronos@gmail.com>
2023-08-13 19:57:11 +0500

    Move from auth header to cookies

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -1,12 +1,12 @@
 import express from "express";
 const router = express.Router();
 import {
-  preSignUp,
-  signup,
-  login,
-  signout,
-  forgotPassword,
-  resetPassword,
+	preSignUp,
+	signup,
+	login,
+	logout,
+	forgotPassword,
+	resetPassword,
 } from "../controller/auth-controller.js";
 
 import passport from "../configs/passport-setup.js";
@@ -14,45 +14,44 @@ import passport from "../configs/passport-setup.js";
 // validators
 import runValidation from "../validators/index.js";
 import {
-  userSignupValidator,
-  userSigninValidator,
-  forgotPasswordValidator,
-  resetPasswordValidator,
+	userSignupValidator,
+	userSigninValidator,
+	forgotPasswordValidator,
+	resetPasswordValidator,
 } from "../validators/auth-validator.js";
 
 // if validation is passed, execute the code in signup and signin controllers
 router.post("/pre-signup", userSignupValidator, runValidation, preSignUp);
 router.post(
-  "/signup",
-  passport.authenticate("pre-signup", { session: false }),
-  signup
+	"/signup",
+	passport.authenticate("pre-signup", { session: false }),
+	signup
 );
 router.post(
-  "/verify-token",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    const token = req.headers.authorization.split(" ")[1];
-    let user: any = req.user;
-    user = user.user;
-    res.json({ token: token, user: user });
-  }
+	"/verify-token",
+	passport.authenticate("jwt", { session: false }),
+	(req, res) => {
+		const token = req.cookies["token"];
+		const userData = req.user;
+		res.json({ user: userData });
+	}
 );
 
 router.post("/login", userSigninValidator, runValidation, login);
-router.get("/signout", signout);
+router.post("/logout", logout);
 router.put(
-  "/forgot-password",
-  forgotPasswordValidator,
-  runValidation,
-  passport.authenticate("jwt", { session: false }),
-  forgotPassword
+	"/forgot-password",
+	forgotPasswordValidator,
+	runValidation,
+	passport.authenticate("jwt", { session: false }),
+	forgotPassword
 );
 router.put(
-  "/reset-password",
-  resetPasswordValidator,
-  runValidation,
-  passport.authenticate("jwt", { session: false }),
-  resetPassword
+	"/reset-password",
+	resetPasswordValidator,
+	runValidation,
+	passport.authenticate("jwt", { session: false }),
+	resetPassword
 );
 
 export default router;

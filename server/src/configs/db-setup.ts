f81077ce72d0dd75cc0446b8196b3010a26f815f
Saad Ahmed <saadahmedcronos@gmail.com>
2023-08-13 19:41:40 +0500

    Refactor envs with run-time config

--- server/src/configs/db-setup.ts
+++ server/src/configs/db-setup.ts
@@ -2,30 +2,32 @@ import mongoose, { ConnectOptions } from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+import { CONNECTION_STRING, DB_NAME } from "./runtime-envs.config.js";
+
 const connectDB = async () => {
-  try {
-    console.log("Connecting to DB...");
-    await mongoose.connect(process.env.CONNECTION_STRING, {
-      dbName: process.env.NODE_ENV === "production" ? "production" : "dev",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    } as ConnectOptions);
-    console.log("DB Connected");
-  } catch (err) {
-    console.log("DB Connection Error: ", err);
-  }
+	try {
+		console.log("Connecting to DB...");
+		await mongoose.connect(CONNECTION_STRING, {
+			dbName: DB_NAME,
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		} as ConnectOptions);
+		console.log(`DB Connected (${DB_NAME})`);
+	} catch (err) {
+		console.log("DB Connection Error: ", err);
+	}
 };
 
 process.on("SIGINT", async () => {
-  await mongoose.connection.close();
-  console.log("\nDB connection closed through app termination");
-  process.exit(0);
+	await mongoose.connection.close();
+	console.log("\nDB connection closed through app termination");
+	process.exit(0);
 });
 
 process.on("SIGTERM", async () => {
-  await mongoose.connection.close();
-  console.log("\nDB connection closed through app termination");
-  process.exit(0);
+	await mongoose.connection.close();
+	console.log("\nDB connection closed through app termination");
+	process.exit(0);
 });
 
 export { connectDB };

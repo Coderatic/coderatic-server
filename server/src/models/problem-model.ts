ceb67d1b827ec4308cceabf410c6c4ae6bccb7fb
Saad Ahmed <saadahmedcronos@gmail.com>
2023-08-13 19:55:28 +0500

    Add slug and time and mem limits along with ORM level validation

--- server/src/models/problem-model.ts
+++ server/src/models/problem-model.ts
@@ -4,58 +4,104 @@ import { ISampleTest } from "./sample-test-model.js";
 import shortId from "shortid";
 
 interface IProblem extends Document {
-  name: string;
-  short_id: string;
-  statement: string;
-  input_format: string;
-  output_format: string;
-  sample_test_sets?: ISampleTest["_id"];
-  hidden_test_sets?: IHiddenTest["_id"];
+	name: string;
+	short_id: string;
+	slug: string;
+	time_lim?: number;
+	mem_lim?: number;
+	statement: string;
+	input_format: string;
+	output_format: string;
+	constraints: string;
+	difficulty?: "Easy" | "Medium" | "Hard";
+	sample_tests?: ISampleTest["_id"];
+	hidden_tests: IHiddenTest["_id"];
 }
 
 const ProblemSchema = new mongoose.Schema<IProblem>(
-  {
-    name: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    short_id: {
-      type: String,
-      unique: true,
-      index: true,
-      default: shortId.generate,
-    },
-    statement: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    input_format: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    output_format: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    sample_test_sets: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "TestSet",
-        required: false,
-      } as ISampleTest["_id"],
-    ],
-    hidden_test_sets: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "TestSet",
-      } as IHiddenTest["_id"],
-    ],
-  },
-  { collection: "problem", timestamps: true }
+	{
+		name: {
+			type: String,
+			trim: true,
+			required: true,
+			maxlength: 50,
+		},
+		short_id: {
+			type: String,
+			unique: true,
+			index: true,
+			default: shortId.generate,
+		},
+		slug: {
+			type: String,
+			unique: true,
+			index: true,
+			required: true,
+			default: function () {
+				return (
+					this.name.toLowerCase().replace(/ /g, "-") +
+					"-" +
+					this.short_id
+				);
+			},
+			maxlength: 50,
+		},
+		time_lim: {
+			type: Number,
+			required: true,
+			min: 1,
+			max: 10,
+		},
+		mem_lim: {
+			type: Number,
+			required: true,
+			min: 16,
+			max: 1024,
+		},
+		statement: {
+			type: String,
+			trim: true,
+			required: true,
+			maxlength: 10000,
+		},
+		input_format: {
+			type: String,
+			trim: true,
+			required: true,
+			maxlength: 1000,
+		},
+		output_format: {
+			type: String,
+			trim: true,
+			required: true,
+			maxlength: 1000,
+		},
+		constraints: {
+			type: String,
+			trim: true,
+			required: true,
+			maxlength: 1000,
+		},
+		difficulty: {
+			type: String,
+			enum: ["Easy", "Medium", "Hard"],
+			required: false,
+		},
+		sample_tests: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "SampleTest",
+			} as ISampleTest["_id"],
+		],
+		hidden_tests: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "HiddenTest",
+				required: true,
+			} as IHiddenTest["_id"],
+		],
+	},
+	{ collection: "problem", timestamps: true }
 );
 
 export default mongoose.model<IProblem>("Problem", ProblemSchema);

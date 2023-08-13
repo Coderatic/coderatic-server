b9c68ab7e2e75b13504437bbd4533a06c97434f3
Saad Ahmed <saadahmedcronos@gmail.com>
2023-08-13 19:42:29 +0500

    Add DB related run-time envs

--- server/src/configs/runtime-envs.config.ts
+++ server/src/configs/runtime-envs.config.ts
@@ -14,13 +14,34 @@ const SERVER_URI =
 
 //DB related
 const CONNECTION_STRING =
-	process.env.PROD_CONNECTION_STRING || process.env.DEV_CONNECTION_STRING;
-const DB_NAME = process.env.NODE_ENV === "production" ? "production" : "dev";
+	process.env.NODE_ENV === "production"
+		? process.env.PROD_CONNECTION_STRING
+		: process.env.DEV_CONNECTION_STRING;
+const DB_NAME =
+	process.env.NODE_ENV === "production" ? "coderatic-prod" : "coderatic-dev";
+
+//Redis
+const REDIS_HOST =
+	process.env.NODE_ENV === "production"
+		? process.env.PROD_REDIS_HOST
+		: process.env.DEV_REDIS_HOST;
+const REDIS_PORT: number =
+	process.env.NODE_ENV === "production"
+		? Number(process.env.PROD_REDIS_PORT)
+		: Number(process.env.DEV_REDIS_PORT);
+const REDIS_PASSWORD =
+	process.env.NODE_ENV === "production"
+		? process.env.PROD_REDIS_PASSWORD
+		: process.env.DEV_REDIS_PASSWORD;
+const REDIS_CONNECTION_STRING =
+	process.env.NODE_ENV === "production"
+		? process.env.PROD_REDIS_CONNECTION_STRING
+		: process.env.DEV_REDIS_CONNECTION_STRING;
 
 // Expiration times
 const PRE_SIGNUP_TOKEN_AGE =
 	process.env.NODE_ENV === "production" ? "10m" : "1d";
-const TOKEN_AGE = process.env.NODE_ENV === "production" ? "10m" : "5s";
+const TOKEN_AGE = process.env.NODE_ENV === "production" ? "10m" : "24h";
 const COOKIE_AGE =
 	process.env.NODE_ENV === "production" ? TEN_MINUTES : ONE_DAY;
 
@@ -34,6 +55,10 @@ export {
 	SERVER_URI,
 	CONNECTION_STRING,
 	DB_NAME,
+	REDIS_HOST,
+	REDIS_PORT,
+	REDIS_PASSWORD,
+	REDIS_CONNECTION_STRING,
 	PRE_SIGNUP_TOKEN_AGE,
 	TOKEN_AGE,
 	COOKIE_AGE,

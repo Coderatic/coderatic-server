35e51e3fe96b973e08b5c92e1fd018ed0796d129
Saad Ahmed <saadahmedcronos@gmail.com>
2023-08-13 19:48:20 +0500

    Make verdicts more verbose

--- server/src/microservices/judge/judge-queue.types.ts
+++ server/src/microservices/judge/judge-queue.types.ts
@@ -1,8 +1,12 @@
-type verdict = {
-  judge_verdict: string;
-  time?: number;
-  memory?: number;
-  test_case_type: string;
+import { JobResult } from "../../controller/types/submission-controller.types.js";
+
+type Verdict = "Accepted" | "Wrong Answer" | "TLE" | "MLE" | "RE" | "IE";
+
+type TestResult = {
+	verdict: Verdict;
+	cpu_time?: number;
+	memory?: number;
 };
 
-export type { verdict };
+export { Verdict };
+export type { TestResult, JobResult };

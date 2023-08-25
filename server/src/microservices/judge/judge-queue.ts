0f25a30905dfec171b3143540c7ecfb866a18160
Saad Ahmed <saadahmedcronos@gmail.com>
2023-08-26 00:38:06 +0500

    Refactor compilation logic

--- server/src/microservices/judge/judge-queue.ts
+++ server/src/microservices/judge/judge-queue.ts
@@ -10,7 +10,7 @@ import SubmissionModel from "../../models/submission-model.js";
 
 //Types
 import { JudgeJob } from "../../controller/types/submission-controller.types.js";
-import { TestResult, Verdict, JobResult } from "./judge-queue.types.js";
+import { TestResult, JobResult } from "./judge-queue.types.js";
 
 // Utils
 import {
@@ -51,60 +51,60 @@ JudgeQueue.process(async (job): Promise<JobResult> => {
 		job.data.problemData;
 	const file_name = uuidv4();
 
-	const src_path = path.join(
-		CWD,
-		`cache/code/${file_name}.${lang.extension}`
-	);
+	const exec_path = path.join(CWD, `cache/code/${file_name}`);
+	const src_path = exec_path + `.${lang.extension}`;
 
 	//Save escaped source code to file
 	const escaped_src = escapeSrc(source_code);
 	fs.writeFileSync(src_path, escaped_src, { flag: "w" });
 
+	let result: JobResult = {
+		verdict: "AC",
+		cpu_time: 0,
+		memory: 0,
+		hidden_tests_results: [],
+		sample_tests_results: [],
+	};
 	//Compile the program (if required)
 	if (lang.is_compiled) {
-		const compile_script = `./compile.sh ${lang.name} ${file_name}`;
-
+		const compile_script = `./compile.sh "${lang.name}" ${file_name}`;
 		try {
 			await runCommand(compile_script, JUDGE_WD);
 		} catch (err) {
-			//cleanup([src_path]);
-			const earlyResult: JobResult = {
-				verdict: "IE",
-				cpu_time: 0,
-				memory: 0,
-				sample_tests_results: [],
-				hidden_tests_results: [],
-			};
-			if (err.exitCode === 1) {
-				earlyResult.verdict = "CE";
-			}
-			return earlyResult;
+			cleanup([src_path]);
+			result.verdict = err.exitCode == 1 ? "CE" : "IE";
 		}
 	}
+	//@ts-ignore
+	if (result?.verdict !== "CE" && result?.verdict !== "IE") {
+		const sample_testPromise: Promise<TestResult[]> = startJudging(
+			file_name,
+			sample_tests,
+			job.data,
+			"sample"
+		);
+		const hidden_testPromise: Promise<TestResult[]> = startJudging(
+			file_name,
+			hidden_tests,
+			job.data,
+			"hidden"
+		);
+		const [sample_test_results, hidden_test_results] = await Promise.all([
+			sample_testPromise,
+			hidden_testPromise,
+		]);
+		cleanup([src_path]);
+		//@ts-ignore
+		lang.is_compiled && result?.verdict !== "CE"
+			? cleanup([exec_path])
+			: null;
 
-	const sample_testPromise: Promise<TestResult[]> = startJudging(
-		file_name,
-		sample_tests,
-		job.data,
-		"sample"
-	);
-	const hidden_testPromise: Promise<TestResult[]> = startJudging(
-		file_name,
-		hidden_tests,
-		job.data,
-		"hidden"
-	);
-	const [sample_test_results, hidden_test_results] = await Promise.all([
-		sample_testPromise,
-		hidden_testPromise,
-	]);
-	//cleanup([src_path, exec_path]);
-
-	const result: JobResult = {
-		...aggregateResults(sample_test_results, hidden_test_results),
-		sample_tests_results: sample_test_results,
-		hidden_tests_results: hidden_test_results,
-	};
+		result = {
+			...aggregateResults(sample_test_results, hidden_test_results),
+			sample_tests_results: sample_test_results,
+			hidden_tests_results: hidden_test_results,
+		};
+	}
 
 	// Save submission to db but don't await (le trolling the user on failed save)
 	const submissionData = job.data.submissionData;
@@ -118,9 +118,8 @@ JudgeQueue.process(async (job): Promise<JobResult> => {
 		verdict: result.verdict,
 		cpu_time: result.cpu_time,
 		memory: result.memory,
-		code_size: Buffer.byteLength(escaped_src, "utf8"),
+		code_size: Buffer.byteLength(source_code, "utf8"),
 	}).save();
-
 	return result;
 });
 

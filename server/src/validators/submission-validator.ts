e65cb6f4328517dde1a5f722eb00c837d5824a67
Saad Ahmed <saadahmedcronos@gmail.com>
2023-08-26 00:41:48 +0500

    Update languages and add validation for getting submissions

--- server/src/validators/submission-validator.ts
+++ server/src/validators/submission-validator.ts
@@ -1,38 +1,56 @@
-import { check, body, header } from "express-validator";
-import Problem from "../models/problem-model.js";
-import User from "../models/problem-model.js";
-import jwt from "jsonwebtoken";
+import { check, body, query } from "express-validator";
 
 const submissionValidator = [
-  body("problem_id").not().isEmpty().withMessage("Problem ID is required"),
-  //body("user_id").not().isEmpty().withMessage("User ID is required"),
-  body("source_code").not().isEmpty().withMessage("Code is required"),
-  body("lang").not().isEmpty().withMessage("Language is required"),
-  body("lang.name").not().isEmpty().withMessage("Language is required"),
-  check("lang.name")
-  .isIn([
-    "c",
-    "cpp",
-    "java",
-    "py3",
-    "javascript",
-      "typescript",
-      "kotlin",
-      "golang",
-      "rust",
-      "csharp",
-      "scala",
-      "haskell",
-    ])
-    .withMessage("Invalid language"),
-  // header("token").not().isEmpty().withMessage("You must be logged-in"),
-  // header("token").custom((value) => {
-  //   jwt.verify(value, process.env.JWT_SECRET, (err, decoded) => {
-  //     if (err) {
-  //       throw new Error("You are not authorized to perform this action.");
-  //     }
-  //   });
-  // }),
+	body("problem_id").not().isEmpty().withMessage("Problem ID is required"),
+	body("source_code").not().isEmpty().withMessage("Code is required"),
+	body("lang").not().isEmpty().withMessage("Language is required"),
+	body("lang.name")
+		.not()
+		.isEmpty()
+		.withMessage("Language name not specified"),
+	check("lang.name")
+		.isIn([
+			"C",
+			"C++ 11",
+			"C++ 14",
+			"C++ 17",
+			"C++ 20",
+			"Java 8",
+			"Java 11",
+			"Python 2",
+			"Python 3",
+			"JavaScript",
+			"TypeScript",
+			"Kotlin",
+			"Golang",
+			"Rust",
+			"C#",
+			"Scala",
+			"Haskell",
+			"Zig",
+			"PHP",
+		])
+		.withMessage("Invalid language"),
 ];
 
-export { submissionValidator };
+const mySubmissionsValidator = [
+	query("problem_id").not().isEmpty().withMessage("Problem ID is required"),
+	query("startingRow")
+		.not()
+		.isEmpty()
+		.withMessage("Starting row is required")
+		.isNumeric()
+		.withMessage("Starting row must be a number")
+		.isInt({ min: 0 })
+		.withMessage("Starting row must be greater than 0"),
+	query("count")
+		.not()
+		.isEmpty()
+		.withMessage("Count is required")
+		.isNumeric()
+		.withMessage("Count must be a number")
+		.isInt({ min: 1 })
+		.withMessage("Count must be greater than 0"),
+];
+
+export { submissionValidator, mySubmissionsValidator };

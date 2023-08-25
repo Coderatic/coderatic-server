bd03d130b9e3627dec066ae3ed3865a70dde614f
Saad Ahmed <saadahmedcronos@gmail.com>
2023-08-26 00:39:04 +0500

    Update language names

--- server/src/microservices/judge/controller-scripts/compile.sh
+++ server/src/microservices/judge/controller-scripts/compile.sh
@@ -6,21 +6,20 @@ bin_name=$src
 code_path="../cache/code"
 
 case "$lang" in
-cpp)
+"C++ 11" | "C++ 14" | "C++ 17" | "C++ 20")
 	g++ "$code_path/$src".cpp -static -O2 -o "$code_path"/"$bin_name"
 	;;
 c)
 	gcc "$code_path/$src".c -static -o "$code_path"/"$bin_name"
 	;;
-rust)
+Rust)
 	rustc "$code_path/$src".rs --crate-type=bin -C prefer-dynamic=no -C target-feature=+crt-static -o "$code_path"/"$bin_name"
+	;;
+golang)
+	go build -o "$code_path"/"$bin_name" "$code_path/$src".go -ldflags "-linkmode 'external' -extldflags '-static'"
 	;;
 *)
 	echo "Language not supported"
 	exit 1
 	;;
 esac
-
-# DOCKER_BUILDKIT=1
-# docker build . -t cpp_img -f cpp.Dockerfile
-# docker build . -t py3_img -f py3.Dockerfile
